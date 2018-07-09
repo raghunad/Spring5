910b045d2def5673a008577235095ab7c811b5a7
Raghu Nadimpalli <varma2801@gmail.com>
2018-07-09 00:20:06 -0400

    Raghu - Updates to connect to MySQL Database

--- Spring5WebApp/src/main/java/com/raghu/spring5webapp/model/Book.java
+++ Spring5WebApp/src/main/java/com/raghu/spring5webapp/model/Book.java
@@ -11,8 +11,6 @@ public class Book {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    //
-
     private String title;
     private String isbn;
     private String publisher;
